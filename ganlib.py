50e3125ec39e38fa8d8104efa596ed999df8a813
Shawn Presser <shawnpresser@gmail.com>
2019-10-28 12:14:38 -0500

    Minor fix

--- ganlib.py
+++ ganlib.py
@@ -11,7 +11,7 @@ def load_model(
     cache_dir = 'cache'):
   session = session or tf.get_default_session()
   with session.as_default():
-    with dnnlib.util.open_url(url, cache_dir='cache') as f:
+    with dnnlib.util.open_url(url, cache_dir=cache_dir) as f:
       _G, _D, _Gs = pickle.load(f)
     G = tflib.Network(_G.name, G_style, **_G.static_kwargs)
     G.copy_vars_from(_G)
@@ -26,7 +26,7 @@ def load_perceptual(
     session = None,
     cache_dir = 'cache'):
   session = session or tf.get_default_session()
-  with dnnlib.util.open_url(url, cache_dir='cache') as f:
+  with dnnlib.util.open_url(url, cache_dir=cache_dir) as f:
     _P = pickle.load(f)
   with session.as_default():
     P = tflib.Network(_P.name, lpips_network, **_P.static_kwargs)
